68bf9c05cd55d029cf418ac1bc12487b75b72342
jirawinj-lang <jirawin.j@rmutsvmail.com>
2026-04-21 11:05:27 +0700

    Update SeenAI.py

--- SeenAI.py
+++ SeenAI.py
@@ -3,3 +3,13 @@ has_high_income = False
 
 if has_good_credit and not has_high_income:
     print("Eligible for load")
+
+
+weight = int(input('Weight: '))
+unit = input('(L)bs or (K)g: ')
+if unit.upper() == "L":
+    converted = weight * 0.45
+    print(f"You are {converted} kilos")
+else:
+    converted = weight / 0.45
+    print(f"You are {converted} pounds")
